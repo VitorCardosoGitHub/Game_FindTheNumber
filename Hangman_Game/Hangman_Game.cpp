6c9b0911fd704c21655b3c67f3a65ecb6d40a195
Vitor Cardoso <vitorcsantos.github@gmail.com>
2025-09-06 18:43:30 -0300

    Displaying the secret word

--- Hangman_Game/Hangman_Game.cpp
+++ Hangman_Game/Hangman_Game.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
-
 
 //Use "mingw32-make forca" to compile the file on Windows.
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute){
     // for (int i = 0; i < PALAVRA_SECRETA.size(); i++){
@@ -23,23 +24,34 @@ bool letra_existe(char chute){
 }
 
 int main () {
-    
-    cout << PALAVRA_SECRETA << endl;
+
+    cout << "\nDecifre a palavra:\n";
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
-    while (nao_acertou && nao_enforcou)
-    {
+    while (nao_acertou && nao_enforcou){   
+        for (char letra : PALAVRA_SECRETA){
+            if (chutou[letra]){
+                cout << letra << " ";
+            }else{
+                cout << "_ ";
+            }
+        }
+        
+        cout << endl;
+        
         char chute;
         cin >> chute;
 
+        chutou [chute] = true;
+
         if (letra_existe(chute))
         {
-            cout << "Voce acertou! Seu chute esta na palavra." << endl;
+            cout << "\nVoce acertou! Seu chute esta na palavra.\n" << endl;
         }
         else{
-            cout << "Voce errou! Seu chute nao esta na palavra." << endl;
+            cout << "\nVoce errou! Seu chute nao esta na palavra.\n" << endl;
         }
         
     }
