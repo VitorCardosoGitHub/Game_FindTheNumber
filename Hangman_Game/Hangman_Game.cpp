77cc4c2d9f2e242301cefc7b45454bce44ceb51c
Vitor Cardoso <vitorcsantos.github@gmail.com>
2025-09-06 19:03:23 -0300

    Adding auto system cls to screen

--- Hangman_Game/Hangman_Game.cpp
+++ Hangman_Game/Hangman_Game.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 
 //Use "mingw32-make forca" to compile the file on Windows.
 
 const string PALAVRA_SECRETA = "MELANCIA";
 map<char, bool> chutou;
+vector<char> chutes_errados;
 
 bool letra_existe(char chute){
     // for (int i = 0; i < PALAVRA_SECRETA.size(); i++){
@@ -25,12 +27,19 @@ bool letra_existe(char chute){
 
 int main () {
 
+    system("cls");
+
+    cout << "\n|*****************************************|" << endl;
+    cout << "|     Seja bem-vindo ao Hangman Game!     |" << endl;
+    cout << "|*****************************************|" << endl;
+
     cout << "\nDecifre a palavra:\n";
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
-    while (nao_acertou && nao_enforcou){   
+    while (nao_acertou && nao_enforcou){
+        
         for (char letra : PALAVRA_SECRETA){
             if (chutou[letra]){
                 cout << letra << " ";
@@ -38,9 +47,16 @@ int main () {
                 cout << "_ ";
             }
         }
+
+        cout << "\n\nChutes errados: ";
+        for (char letra : chutes_errados){
+            cout << letra << ", ";
+        }
         
         cout << endl;
         
+        cout << "\nDigite seu chute: ";
+
         char chute;
         cin >> chute;
 
@@ -52,7 +68,10 @@ int main () {
         }
         else{
             cout << "\nVoce errou! Seu chute nao esta na palavra.\n" << endl;
+            chutes_errados.push_back(chute);
         }
+
+        system("cls");
         
     }
     
