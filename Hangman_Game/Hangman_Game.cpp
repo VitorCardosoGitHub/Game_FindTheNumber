a1cb8670376603264a4f62fef9fd89cc77e723a3
Vitor Cardoso <vitorcsantos.github@gmail.com>
2025-09-06 17:00:08 -0300

    Creating the final game conditions

--- Hangman_Game/Hangman_Game.cpp
+++ Hangman_Game/Hangman_Game.cpp
@@ -2,7 +2,46 @@
 #include <string>
 using namespace std;
 
+
+//Use "mingw32-make forca" to compile the file on Windows.
+
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute){
+    // for (int i = 0; i < PALAVRA_SECRETA.size(); i++){
+    //     if (chute == PALAVRA_SECRETA[i]){
+    //         return true;
+    //     }
+    // }
+
+    for (char letra : PALAVRA_SECRETA){
+        if (chute == letra){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main () {
-    string palavra_secreta = "Melancia";
-    cout << palavra_secreta << endl;
+    
+    cout << PALAVRA_SECRETA << endl;
+
+    bool nao_acertou = true;
+    bool nao_enforcou = true;
+
+    while (nao_acertou && nao_enforcou)
+    {
+        char chute;
+        cin >> chute;
+
+        if (letra_existe(chute))
+        {
+            cout << "Voce acertou! Seu chute esta na palavra." << endl;
+        }
+        else{
+            cout << "Voce errou! Seu chute nao esta na palavra." << endl;
+        }
+        
+    }
+    
 }
